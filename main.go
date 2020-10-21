a6aa1e7e082a2dd4478138407bcb7719263cdfae
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-10-21 19:32:46 +0300

    added filewalk tests

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
 	fmt.Println(hello())
+	fmt.Println(filePathWalkDir("."))
 }
 
 func hello() string {
 	return "Hello, World"
+}
+
+func filePathWalkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return err
+	})
+	return files, err
 }
--- main_test.go
+++ main_test.go
@@ -6,8 +6,26 @@ func TestHello(t *testing.T) {
 	resultFromHello := hello()
 
 	if resultFromHello != "Hello, World" {
-		t.Errorf("Expected result is Hello, World but %s given", resultFromHello)
+		t.Errorf("Expected result is 'Hello, World' but %s is given", resultFromHello)
 	} else {
 		t.Logf("Success.")
+	}
+}
+
+func TestFilePathFinder_ShouldReturnFiles(t *testing.T) {
+	files, err := filePathWalkDir("test/")
+	if err != nil {
+		t.Errorf("Shouldn't error")
+	}
+
+	if len(files) < 0 {
+		t.Errorf("Files length should be greater than 0")
+	}
+}
+
+func TestFilePathFinder_NotFound(t *testing.T) {
+	_, err := filePathWalkDir("empty/")
+	if err == nil {
+		t.Errorf("Expected error.")
 	}
 }
