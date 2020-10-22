46ddaaf059ab6747062b0e8e25f210bd87dab564
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-10-22 21:12:58 +0300

    plugin.xml properties added as a struct but not filled correctly

--- parser/xml_parser.go
+++ parser/xml_parser.go
@@ -9,40 +9,32 @@ import (
 
 // Cordova plugin.xml
 type Plugin struct {
-	Name        string   `xml:"name"`
-	Description string   `xml:"description"`
-	Author      string   `xml:"author"`
-	Keywords    []string `xml:"keywords"`
-	License     string   `xml:"license"`
-	JsModule    JSModule `xml:"js-module"`
-	SourceFile  SourceFile
+	ID           string   `xml:"id, Attr"`
+	Version      string   `xml:"version, Attr"`
+	Xmlns        string   `xml:"xmlns, Attr"`
+	XmlnsAndroid string   `xml:"xmlns:android, Attr"`
+	Name         string   `xml:"name"`
+	Description  string   `xml:"description"`
+	License      string   `xml:"license"`
+	Keywords     []string `xml:"keywords"`
+	Author       string   `xml:"author"`
+	Engines      []Engine `xml:"engines"`
+	JsModule     JSModule `xml:"js-module"`
+	Platform     Platform `xml:"platform"`
+}
+
+type Platform struct {
+}
+
+type Engine struct {
 }
 
 type SourceFile struct {
-	Src       string
+	Src       string `xml:"src,Attr"`
 	TargetDir string
 }
 type JSModule struct {
 	clobbers string `xml:"clobbers"`
-}
-
-// -------------
-type Users struct {
-	XMLName xml.Name `xml::"users"`
-	Users   []User   `xml:"user"`
-}
-type User struct {
-	XMLName xml.Name `xml:"user"`
-	Type    string   `xml:"type,attr"`
-	Name    string   `xml:"name"`
-	Social  Social   `xml:"social"`
-}
-
-type Social struct {
-	XMLName  xml.Name `xml:"social"`
-	Facebook string   `xml:"facebook"`
-	Twitter  string   `xml:"twitter"`
-	Youtube  string   `xml:"youtube"`
 }
 
 func Run() {
