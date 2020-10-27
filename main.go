38a8d6ff1b899550866b4e1c0ea9f598d35e6822
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-10-27 19:16:54 +0300

    cli structre started

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/bilginyuksel/cordova-plugin-helper/parser"
+	"github.com/bilginyuksel/cordova-plugin-helper/reader"
+
 	"github.com/alecthomas/kong"
 	//"github.com/bilginyuksel/cordova-plugin-helper/writer"
 )
 
 func main() {
 	prepareCliParser()
-	// plg, _ := parser.ParseXML("parser/plugin.xml")
-	// javaFiles, _ := reader.FilePathWalkDir("src")
-	// plg.Platform.NewSourceFrom(javaFiles)
-	// jsModules, _ := reader.FilePathWalkDir("www")
-	// plg.NewJsModulesFrom(jsModules)
-	// parser.CreateXML(plg, "plg.xml")
 }
 
 func prepareCliParser() {
@@ -28,22 +27,33 @@ type Context struct {
 
 // PluginXMLCmd ...
 type PluginXMLCmd struct {
-	Path string `help:"String to path"`
-	Sync bool   `help:"Sync plugin.xml file. This command will search the related directories and if it finds any missing or unnecessary field it will add or delete automatically."`
+	Path string `help:"The folder path to sync."`
 }
 
 // AddLicenseCmd ...
 type AddLicenseCmd struct {
-	Paths          []string `arg optional name:"path" help:"Paths to list." type:"path"`
-	FileExtensions []string `arg optional name:"extension" help:"Instead of giving every files path, just give file extensions here."`
+	Paths          []string `name:"path" help:"Paths to list." type:"path"`
+	FileExtensions []string `name:"extension" help:"Instead of giving every files path, just give file extensions here."`
 	License        string   `help:"License file path to use."`
 }
 
 // Run ...
-// func (pl *PluginXMLCmd) Run(ctx *Context) error {
-// 	fmt.Println(pl)
-// 	return nil
-// }
+func (pl *PluginXMLCmd) Run(ctx *Context) error {
+	if pl.Path == "" {
+		pl.Path = "."
+	}
+	files, _ := reader.FilePathWalkDir(pl.Path)
+	fmt.Println(files)
+	plg, _ := parser.ParseXML(fmt.Sprintf("%s/plugin.xml", pl.Path))
+	fmt.Println(plg)
+	// javaFiles, _ := reader.FilePathWalkDir("src")
+	// plg.Platform.NewSourceFrom(javaFiles)
+	// jsModules, _ := reader.FilePathWalkDir("www")
+	// plg.NewJsModulesFrom(jsModules)
+	// parser.CreateXML(plg, "plugin.xml")
+	// fmt.Println(pl)
+	return nil
+}
 
 // Run ...
 // func (l *AddLicenseCmd) Run(ctx *Context) error {
@@ -54,6 +64,6 @@ type AddLicenseCmd struct {
 var cli struct {
 	Debug bool `help:"Enable debug mode."`
 
-	PluginXML  PluginXMLCmd  `cmd help:"You can use the functions in that command to manipulate plugin.xml file under cordova plugin root project directory."`
-	AddLicense AddLicenseCmd `cmd help:"Add license to files."`
+	PluginXML  PluginXMLCmd  `cmd:"" help:"You can use the functions in that command to manipulate plugin.xml file under cordova plugin root project directory."`
+	AddLicense AddLicenseCmd `cmd:"" help:"Add license to files."`
 }
