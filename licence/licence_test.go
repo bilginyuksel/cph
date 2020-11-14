45ae56e07c661a403cbbc3e01a500a6e98b64550
Safa Yildirim 84171622 <safa.yildirim@huawei.com>
2020-11-15 01:52:25 +0300

    parse implementation is started

--- licence/licence_test.go
+++ licence/licence_test.go
@@ -5,20 +5,6 @@ import (
 	"testing"
 )
 
-const licence = `	Copyright %d %s
-
-	Licensed under the Apache License, Version 2.0 (the "License")
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-		https://www.apache.org/licenses/LICENSE-2.0
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.`
-
 const falseLicence = `Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License")
@@ -32,7 +18,7 @@ See the License for the specific language governing permissions and
 limitations under the License.`
 
 func TestCompareLicenceSimilarity_ZeroDifference(t *testing.T) {
-	if 0 != comparePotentialLicenceToActualLicence(licence) {
+	if 0 != comparePotentialLicenceToActualLicence(LICENCE) {
 		t.Error()
 	}
 }
@@ -130,7 +116,7 @@ func TestDeleteInvalidLicence_DeleteInvalid(t *testing.T) {
 }
 
 func TestFindCommentedInvalidLicenceToDelete_Similar(t *testing.T) {
-	licSim := findCommentedInvalidLicenceToDelete("/*"+licence+"*/", 0.3)
+	licSim := findCommentedInvalidLicenceToDelete("/*"+LICENCE+"*/", 0.3)
 	fmt.Printf("Licsim prob= %f", licSim.prob)
 	if !licSim.similar {
 		t.Error()
